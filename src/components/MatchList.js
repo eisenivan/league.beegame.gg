0e2a85c2c25213ae3f91ba1f9c5f2f4ec5d3e064
eisenivan <ivan.eisenberg@gmail.com>
2021-03-21 12:52:46 -0500

    handle incomplete data better in matchList

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -8,8 +8,20 @@ function MatchList ({ matches = [] }) {
         ? matches.map((match) => (
           <div key={`${match.start_time}-${match.home.name}-${match.away.name}`}>
             <h3 className='text-lg'><strong className='text-yellow-3'>{match.home.name}</strong> <em className='text-sm'>({match.home.wins} - {match.home.losses})</em> vs. <strong className='text-blue-3'>{match.away.name}</strong> <em className='text-sm'>({match.away.wins} - {match.away.losses})</em> </h3>
-            <a className='block' rel='noreferrer' target='_blank' href={match.primary_caster.stream_link}>{match.primary_caster.name} ft. {match.secondary_casters.join(', ')}</a>
-            <span className='text-sm'>{moment(match.start_time).tz('America/New_York').format('MM/DD/YYYY h:mm a')} ET</span>
+            {
+              match.primary_caster
+                ? (
+                  <a className='block' rel='noreferrer' target='_blank' href={match.primary_caster.stream_link}>{match.primary_caster.name} ft. {match.secondary_casters.join(', ')}</a>
+                ) : null
+            }
+
+            {
+              match.primary_caster
+                ? (
+                  <span className='text-sm'>{moment(match.start_time).tz('America/New_York').format('MM/DD/YYYY h:mm a')} ET</span>
+                ) : null
+            }
+
           </div>
         ))
         : <h3 className='text-lg'>No scheduled matches at this time</h3>
