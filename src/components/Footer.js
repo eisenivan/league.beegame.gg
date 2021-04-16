45e7cae7f920dafee97a9d77cb7b277595929691
ianfitzpatrick1 <ianfitz@gmail.com>
2021-04-16 18:54:46 -0500

    Decreased font size of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer () {
   return (
     <footer className='p-2 my-10 lg:px-8'>
-      <p className='text-center text-gray-1'>This site and its ventures are unaffiliated with LiquidBit, and are not official productions for Killer Queen Black</p>
-      <div className='text-sm text-center text-white'>With ❤️ from Hattrickswayze, Dadcore, Prosive, Bee'sKnees, Jame, and many more</div>
+      <p className='text-sm text-center text-gray-1'>This site and its ventures are unaffiliated with LiquidBit, and are not official productions for Killer Queen Black</p>
+      <div className='mt-2 text-sm text-center text-gray-1'>With ❤️ from Hattrickswayze, Dadcore, Prosive, Bee'sKnees, Jame, and many more</div>
     </footer>
   )
 }
