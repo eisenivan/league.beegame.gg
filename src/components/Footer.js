f43b87cff273655fe58b44794f8439edc56eaaab
eisenivan <ivan.eisenberg@gmail.com>
2021-04-20 16:34:17 -0500

    get Shenanigans his due

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer () {
   return (
     <footer className='p-2 my-10 lg:px-8'>
       <p className='text-sm text-center text-gray-1'>This site and its ventures are unaffiliated with LiquidBit, and are not official productions for Killer Queen Black</p>
-      <div className='mt-2 text-sm text-center text-gray-1'>With ❤️ from Hattrickswayze, Dadcore, Prosive, Bee'sKnees, Jame, and many more</div>
+      <div className='mt-2 text-sm text-center text-gray-1'>With ❤️ from Hattrickswayze, Dadcore, Prosive, Bee'sKnees, Jame, Shenanigans, and many more</div>
     </footer>
   )
 }
