079fdc1b0010ca8bbc879d3e1b2f969b0d3b56db
eisenivan <ivan.eisenberg@gmail.com>
2021-04-30 11:05:59 -0500

    extend id and player name token life

--- src/modules/set-tokens.js
+++ src/modules/set-tokens.js
@@ -1,4 +1,5 @@
 import cookie from 'react-cookies'
+import moment from 'moment'
 import fetch from './fetch-with-headers'
 import getApiUrl from './get-api-url'
 import handleError from './handle-error'
@@ -7,9 +8,10 @@ function setUserCookies () {
   fetch(`${getApiUrl()}me/?format=json`)
     .then(data => data.json())
     .then((meJson) => {
+      const expires = new Date(moment().add(6, 'months').toDate())
       if (meJson.id) {
-        cookie.save('userId', meJson.player.id, { path: '/', secure: !process.env.NODE_ENV === 'development' })
-        cookie.save('name', meJson.first_name, { path: '/', secure: !process.env.NODE_ENV === 'development' })
+        cookie.save('userId', meJson.player.id, { path: '/', secure: !process.env.NODE_ENV === 'development', expires })
+        cookie.save('name', meJson.first_name, { path: '/', secure: !process.env.NODE_ENV === 'development', expires })
       }
     })
     .catch(handleError)
