88b29e1660c579bcb9319979c49c604713552344
ianfitzpatrick1 <ianfitz@gmail.com>
2021-04-30 03:37:01 -0500

    Took shot at calendar entry styling

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -35,17 +35,18 @@ const EventItem = styled.div`
 function SingleEvent ({ event }) {
   return (
     <EventItem key={`${event.home.name}-${event.away.name}-${event.start_time}`}>
-      <p className='text-xs italic'>{formatTime(event.start_time)}</p>
-      <p className='text-xs font-bold'>{`${event.home.name} vs. ${event.away.name}`}</p>
+      <p className='text-xs text-yellow-1'>{formatTime(event.start_time)}</p>
+      <div class="inline-block rounded px-1 py-1 -ml-1 bg-gray-2 text-gray-400 w-full md:w-auto ">
+        <p className='inline-block px-1 py-1 -ml-1 text-xs font-bold text-white'>{`${event.home.name} vs. ${event.away.name}`}</p>
 
-      { event.circuit
-        ? <p className='mt-1 text-xs italic'><Link className='text-gray-1' to={`/circuits/${event.circuit.id}/`}>{event.circuit.name}</Link></p>
-        : null }
+        { event.circuit
+          ? <p className='mt-1 text-xs'><Link className='text-gray-1' to={`/circuits/${event.circuit.id}/`}>{event.circuit.name} Circuit</Link></p>
+          : null }
 
-      { event.primary_caster
-        ? <a target='_blank' rel='noreferrer' className='flex items-center text-xs text-purple-500' href={event.primary_caster.stream_link}><svg class="mr-1 w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <defs/> <path fill-rule="evenodd" d="M2.149 0L.537 4.119v16.836h5.731V24h3.224l3.045-3.045h4.657l6.269-6.269V0H2.149zm19.164 13.612l-3.582 3.582H12l-3.045 3.045v-3.045H4.119V2.149h17.194v11.463zm-3.582-7.343v6.262h-2.149V6.269h2.149zm-5.731 0v6.262H9.851V6.269H12z" clip-rule="evenodd"/></svg>{event.primary_caster.name}</a>
-        : <span className='text-xs italic'>Looking for caster</span> }
-
+        { event.primary_caster
+          ? <a target='_blank' rel='noreferrer' className='flex items-center text-xs text-purple-500' href={event.primary_caster.stream_link}><svg class="mr-1 w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <defs/> <path fill-rule="evenodd" d="M2.149 0L.537 4.119v16.836h5.731V24h3.224l3.045-3.045h4.657l6.269-6.269V0H2.149zm19.164 13.612l-3.582 3.582H12l-3.045 3.045v-3.045H4.119V2.149h17.194v11.463zm-3.582-7.343v6.262h-2.149V6.269h2.149zm-5.731 0v6.262H9.851V6.269H12z" clip-rule="evenodd"/></svg>{event.primary_caster.name}</a>
+          : <span className='text-xs italic'>Looking for caster</span> }
+      </div>
     </EventItem>
   )
 }
