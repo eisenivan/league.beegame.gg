b28c8e022e92e26df5d91dc6bccf1ef2241fa983
ianfitzpatrick1 <ianfitz@gmail.com>
2021-05-14 21:03:25 -0500

    Added twitch link to all match boxes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -322,7 +322,16 @@ function Home () {
                           ? (
                             playerMatches.map((match) => (
                               <MatchBox key={`match-${match.id}`} match={match}>
-                                <span className='justify-end flex-grow block text-sm'>
+                                <span className='flex items-center justify-end flex-grow text-sm'>
+
+                                { match.primary_caster
+                                        ? <a target='_blank' rel='noreferrer' className='flex items-center mr-4 text-xs leading-loose text-purple-400' href={match.primary_caster.stream_link}>
+                                            <svg className='w-3 h-3 mr-1' fill='currentColor' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'> <defs /> <path fillRule='evenodd' d='M2.149 0L.537 4.119v16.836h5.731V24h3.224l3.045-3.045h4.657l6.269-6.269V0H2.149zm19.164 13.612l-3.582 3.582H12l-3.045 3.045v-3.045H4.119V2.149h17.194v11.463zm-3.582-7.343v6.262h-2.149V6.269h2.149zm-5.731 0v6.262H9.851V6.269H12z' clipRule='evenodd' /></svg>
+                                            {match.primary_caster.name}
+                                          </a>
+                                        : <p className='text-xs italic'>Looking for caster</p>
+                                      }
+
                                   { match.start_time
                                     ? (
                                       <>
