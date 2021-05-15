ba8b4573585b946caed3b809c825aa0f89bed2de
ianfitzpatrick1 <ianfitz@gmail.com>
2021-05-14 20:50:37 -0500

    Microwave proofed matchbox w/ trophy

--- src/components/elements.js
+++ src/components/elements.js
@@ -10,11 +10,11 @@ export function MatchBox ({ match, children }) {
     <div key={`${match.home.name}${match.away.name}${match.id}`} className='max-w-lg mt-1 mb-4 shadow-xl'>
       <div className='text-2xl uppercase font-head'>
         <div className='flex justify-between p-4 truncate text-gray-1 bg-blue-2 text-shadow shadow-match'>
-          <RouterLink className='flex text-white truncate' to={`/teams/${match.away.id}/`}>
-            {match.away.name}
+          <RouterLink className='flex items-center text-white w-112' to={`/teams/${match.away.id}/`}>
+            <span class="truncate">{match.away.name}</span>
 
             { match.result && match.result.winner == match.away.name
-              ? <span class="ml-4" style={{fontSize: '15px'}}>🏆</span>
+              ? <svg class="ml-4 w-5 text-yellow-3 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 1456.2 1161.79"><def/><path class="" fill="currentColor" d="M1360.56 626.71c127.52-127.52 127.52-335 0-462.51a325.16 325.16 0 00-107.75-71.57q2-27.11 2.84-54.35A37.21 37.21 0 001218.4 0H237.8a37.21 37.21 0 00-37.25 38.28q.82 27.23 2.84 54.35A325.16 325.16 0 0095.64 164.2c-127.52 127.52-127.52 335 0 462.51 62.66 62.67 189.09 139.61 307.44 187.1 46.74 18.76 100 36.14 152.16 44.45a1210.29 1210.29 0 01-147.7 253.48c-15.48 20.39-1.27 50 24 50h593.16c25.25 0 39.46-29.65 24-50A1210.29 1210.29 0 01901 858.26c52.11-8.31 105.43-25.69 152.16-44.45 118.31-47.49 244.73-124.43 307.4-187.1zM166.83 555.52c-88.26-88.26-88.26-231.87 0-320.13a225.73 225.73 0 0148.7-37C247.16 400.98 325 588.84 430.2 716.17c-115.29-47.83-219.05-116.28-263.37-160.65zm1073.84-357.16a225.73 225.73 0 0148.7 37c88.26 88.26 88.26 231.87 0 320.13-44.27 44.4-148.08 112.85-263.37 160.65 105.2-127.33 183.04-315.19 214.67-517.78z"/></svg>
               : null 
             }
           </RouterLink>
@@ -25,11 +25,11 @@ export function MatchBox ({ match, children }) {
 
         </div>
         <div className='flex justify-between p-4 text-gray-1 bg-yellow-2 ellipsis text-shadow shadow-match'>
-          <RouterLink className='text-white truncate' to={`/teams/${match.home.id}/`}>
+          <RouterLink className='flex items-center text-white truncate w-112' to={`/teams/${match.home.id}/`}>
             {match.home.name}
 
             { match.result && match.result.winner == match.home.name
-              ? <span class="ml-4" style={{fontSize: '15px'}}>🏆</span>
+              ? <svg class="ml-4 w-5 text-yellow-3" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 1456.2 1161.79"><def/><path class="" fill="currentColor" d="M1360.56 626.71c127.52-127.52 127.52-335 0-462.51a325.16 325.16 0 00-107.75-71.57q2-27.11 2.84-54.35A37.21 37.21 0 001218.4 0H237.8a37.21 37.21 0 00-37.25 38.28q.82 27.23 2.84 54.35A325.16 325.16 0 0095.64 164.2c-127.52 127.52-127.52 335 0 462.51 62.66 62.67 189.09 139.61 307.44 187.1 46.74 18.76 100 36.14 152.16 44.45a1210.29 1210.29 0 01-147.7 253.48c-15.48 20.39-1.27 50 24 50h593.16c25.25 0 39.46-29.65 24-50A1210.29 1210.29 0 01901 858.26c52.11-8.31 105.43-25.69 152.16-44.45 118.31-47.49 244.73-124.43 307.4-187.1zM166.83 555.52c-88.26-88.26-88.26-231.87 0-320.13a225.73 225.73 0 0148.7-37C247.16 400.98 325 588.84 430.2 716.17c-115.29-47.83-219.05-116.28-263.37-160.65zm1073.84-357.16a225.73 225.73 0 0148.7 37c88.26 88.26 88.26 231.87 0 320.13-44.27 44.4-148.08 112.85-263.37 160.65 105.2-127.33 183.04-315.19 214.67-517.78z"/></svg>
               : null 
             }
 
